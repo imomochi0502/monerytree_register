0304030e58663a4ae878758bbd64800a78030c9e
imomochi <imomochi0502@gmail.com>
2022-12-04 18:02:37 +0900

    Return client only

--- mt_register/lib/gspread_func.py
+++ mt_register/lib/gspread_func.py
@@ -1,15 +1,13 @@
 import gspread
-from httplib2 import Credentials
 from oauth2client.service_account import ServiceAccountCredentials
 
 
-def connect_gspread(jsonf,key):
+def connect_gspread(jsonf, spred_sheet_key):
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
     credentials = ServiceAccountCredentials.from_json_keyfile_name(jsonf, scope)
     gc = gspread.authorize(credentials)
-    SPREADSHEET_KEY = key
-    worksheet = gc.open_by_key(SPREADSHEET_KEY).sheet1
-    return worksheet
+    worksheet_client = gc.open_by_key(spred_sheet_key)
+    return worksheet_client
 
 # https://qiita.com/yH3PO4/items/6517481b1e7128ca7b36
 def cellsto2darray(cells, col):  # colは列の数
